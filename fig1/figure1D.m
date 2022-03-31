d2ad6dd84109ce59913d6ea676dd1c451717925c
niklas <niklas.farke@cmfi.uni-tuebingen.de>
2022-03-31 11:13:06 +0200

    figure1D commented

--- fig1/figure1D.m
+++ fig1/figure1D.m
@@ -1,8 +1,12 @@
+
+%plot % RSD in negative mode and positive mode
 [out1,out2] = xlsread('Supplements1','Analytical_Standards');
 
+%get pos error
 pos_error = out1(1:160,18)*100;
 pos_picked = out1(1:160,16);
 
+%get neg error
 neg_error = out1(1:160,25)*100;
 neg_picked = out1(1:160,23);
 
@@ -20,9 +24,8 @@ r = 2.15 - 0.3*rand(160,1);
 scatter(r,neg_error,10,'filled','b')
 ylim([0 100])
 hold on
+%plot the mean
 plot(mean(errors,'omitnan'),'dg')
-
 set(gca,'yscale','log')
-hold on
 
 
