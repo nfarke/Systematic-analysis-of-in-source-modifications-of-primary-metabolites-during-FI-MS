4688a9f00e7ea5f69d1fe78b733cc704c3f08399
niklas <niklas.farke@cmfi.uni-tuebingen.de>
2022-06-02 12:44:15 +0200

    changed figure1D to figure1F, changed colors and colored ionization mode in heatmap

--- fig1/figure1F.m
+++ fig1/figure1F.m
@@ -45,14 +45,19 @@ new_data2 = single(new_data1 > 3);
 id1 = strcmp('[M+H]+',adductsX);
 id2 = strcmp('[M-H]-',adductsX);
 idx = [find(id1);find(id2);];
+
+new_data2 = new_data2.*id1*1 + new_data2.*id2*-1;
 new_data2 = new_data2(idx,idx);
+
 column_labels   = standard_abbr(idx);
 row_labels =  preferred_adducts(idx);
 
+
+
 %plot heatmap
-r = [240 135 5]/255;        %# start
-w = [246 225 181]/255;      %# middle
-b = [0 158 227]/255;       %# end
+r = [26 181 177]/255;        %# start
+w = [230 230 230]/255;      %# middle
+b = [166 26 181]/255;       %# end
 %# colormap of size 64-by-3, ranging from red -> white -> blue
 c1 = zeros(32,3); c2 = zeros(32,3);
 for i=1:3
@@ -64,8 +69,8 @@ aaa = HeatMap(new_data2,'RowLabels',row_labels,'ColumnLabels',column_labels,'Col
 
 
 %%
-num_unspec_targets = sum(sum(new_data2)) - sum(diag(new_data2));
-only_offdiag = new_data2 - diag(diag(new_data2));
+num_unspec_targets = sum(sum(abs(new_data2))) - sum(diag(abs(new_data2)));
+only_offdiag = abs(new_data2) - diag(diag(abs(new_data2)));
 num_standards_w_offtargets = sum(only_offdiag,1);
 [numoff, sortid] = sort(num_standards_w_offtargets,'descend');
 
